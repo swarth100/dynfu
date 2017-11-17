69fbd23494cca2e7ab0ef9cefddbc46c48658d1f
Alberto Spina <as12015@ic.ac.uk>
2017-11-17 13:23:52 +0000

    .gitlab-ci.yml: Add test subdir to deploy phase
    .gitlab-ci.yml: Refactor binary path for test files
    
    test/dualQuaternion/CMakeLists.txt: Refactor attempting to installl in bin
    
    .gitlab-ci.yml: Remove dynFu test cpy for Dual Quaternion
    
    CMakeLists.txt: Enable testing in CMakelists
    
    test/dualQuaternion/CMakeLists.txt: Refactor make_tests
    
    test/dualQuaternion/CMakeLists.txt: Refactor test name
    
    test/dualQuaternion/CMakeLists.txt: Refactor install to bin
    
    .gitlab-ci.yml: Add debugs to yml
    
    test/: Refactor to now build in src directory

---
+++ test/quaternion.cpp
@@ -0,0 +1,46 @@
+#include <gtest/gtest.h>
+
+/* The fixture for testing class Quaternion. */
+class DualQuaternionTest : public ::testing::Test {
+protected:
+    /* You can remove any or all of the following functions if its body
+     * is empty. */
+
+    /* You can do set-up work for each test here. */
+    DualQuaternionTest() {}
+
+    /* You can do clean-up work that doesn't throw exceptions here. */
+    virtual ~DualQuaternionTest() {}
+
+    /* If the constructor and destructor are not enough for setting up
+     * and cleaning up each test, you can define the following methods: */
+
+    /* Code here will be called immediately after the constructor (right
+     * before each test). */
+    virtual void SetUp() {}
+
+    /* Code here will be called immediately after each test (right
+     * before the destructor). */
+    virtual void TearDown() {}
+
+    /* Objects declared here can be used by all tests in the test case for Foo. */
+};
+
+/* */
+TEST_F(DualQuaternionTest, TestReal) {
+    /*
+    DualQuaternion<float> dq1(0, 90, 90, 0.0, 0.0, 0.0);
+    DualQuaternion<float> dq2(0, 90, 90, 0.0, 0.0, 0.0);
+
+    DualQuaternion<float> dqSum = dq1 + dq2;
+
+    DualQuaternion<float> dqRes(2.0, 2.0, 2.0, 0.0, 0.0, 0.0);
+
+    ASSERT_FLOAT_EQ(dqSum.getReal().R_component_1(), dqRes.getReal().R_component_1());
+    */
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
