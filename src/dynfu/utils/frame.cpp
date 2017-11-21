d512f350db278eed7f4b9ed1ccba0d10fc765a00
Daniel Grzech <dgrzech@rapidminer.com>
2017-11-21 23:05:21 +0000

    src/dynfu/utils/frame.cpp: Add Frame implementation

--- src/dynfu/utils/frame.cpp
+++ src/dynfu/utils/frame.cpp
@@ -1,16 +1,18 @@
 #include <dynfu/utils/frame.hpp>
 
-/* TODO: Add comment */
-Frame::Frame(void *addIt) {}
+Frame(int id, std::vector<cv::Vec3f> vertices, std::vector<cv::Vec3f> normals) {
+    this.id        = id;
+    this->vertices = vertices;
+    this->normals  = normals;
+}
 
-/* TODO: Add comment */
-Frame::~Frame() {}
+~Frame() {
+    delete vertices;
+    delete normals;
+}
 
-/* TODO: Add comment */
-void Frame::getId() {}
+int getId() { return this.id; }
 
-/* TODO: Add comment */
-void Frame::getVertices() {}
+std::vector<cv::Vec3f> getVertices() { return this->vertices; }
 
-/* TODO: Add comment */
-void Frame::getNormals() {}
+std::vector<cv::Vec3f> getNormals() { return this->normals; }
