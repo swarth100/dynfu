17cd7c83ebe5263f90a0d682eaf3b5af7cf01f98
Riku Murai <riku.murai15@imperial.ac.uk>
2017-11-22 23:14:42 +0000

    src/dynfu/dyn_fusion.cpp: Get the data for the canonical frame

--- src/dynfu/dyn_fusion.cpp
+++ src/dynfu/dyn_fusion.cpp
@@ -1,5 +1,6 @@
 #include <dynfu/dyn_fusion.hpp>
 
+DynFusion::DynFusion() = default;
 /* We initialise the dynamic fusion with the initals vertices and normals */
 DynFusion::DynFusion(std::vector<cv::Vec3f> vertices, std::vector<cv::Vec3f> /* normals */) {
     /* Sample the deformation nodes */
@@ -20,17 +21,32 @@ DynFusion::DynFusion(std::vector<cv::Vec3f> vertices, std::vector<cv::Vec3f> /* 
 
 void DynFusion::init(kfusion::cuda::Cloud &vertices) {
     cv::Mat cloudHost;
-    cloudHost.create(vertices.rows(), vertices.cols(), CV_8UC4);
-    vertices.download(cloudHost.ptr<void>(), cloudHost.step);
+    cloudHost.create(vertices.rows(), vertices.cols(), CV_32FC4);
+    vertices.download(cloudHost.ptr<kfusion::Point>(), cloudHost.step);
     std::vector<cv::Vec3f> canonical(cloudHost.rows * cloudHost.cols);
 
     for (int y = 0; y < cloudHost.cols; ++y) {
         for (int x = 0; x < cloudHost.rows; ++x) {
             auto point = cloudHost.at<kfusion::Point>(x, y);
             if (!(std::isnan(point.x) || std::isnan(point.y) || std::isnan(point.z)))  {
-                canonical[x + cloudHost.cols * y] = cv::Vec3f(point.x, point.y, point.z);
+                canonical[x + cloudHost.rows * y] = cv::Vec3f(point.x, point.y, point.z);
             }
         }
+    }
+
+    /* Sample the deformation nodes */
+    int steps = 50;
+    std::vector<std::shared_ptr<Node>> deformationNodes;
+    for (int i = 0; i < canonical.size() - steps; i += steps) {
+        auto dq = std::make_shared<DualQuaternion<float>>(0.f, 0.f, 0.f, 0.f, 0.f, 0.f);
+        deformationNodes.push_back(std::make_shared<Node>(canonical[i], dq, 0.f));
+    }
+    /* Initialise the warp field with the inital frames vertices */
+    warpfield = std::make_shared<Warpfield>();
+    warpfield->init(deformationNodes);
+
+    for (auto node : deformationNodes) {
+        node->setNeighbours(warpfield->findNeighbors(KNN, node));
     }
 }
 
