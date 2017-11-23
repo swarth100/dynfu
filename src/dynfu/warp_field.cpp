52c6f6c2d4447046b67731bfe2fc0953677d2812
Alberto Spina <as12015@ic.ac.uk>
2017-11-23 13:21:43 +0000

    src/dynfu/warp_field.cpp: Add PCL logging methods

--- src/dynfu/warp_field.cpp
+++ src/dynfu/warp_field.cpp
@@ -1,8 +1,13 @@
 #include <dynfu/warp_field.hpp>
 
-/* sys headers */
+/* Sys Headers */
 #include <cmath>
 #include <ctgmath>
+#include <string>
+
+/* PCL Headers */
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
 
 /* TODO: Add comment */
 Warpfield::Warpfield() = default;
@@ -21,6 +26,10 @@ void Warpfield::init(std::vector<std::shared_ptr<Node>>& nodes) {
     for (auto node : this->nodes) {
         deformationNodesPosition.push_back(node->getPosition());
     }
+
+    /* Save the deformation Nodes to PCL format */
+    this->saveToPcl(deformationNodesPosition);
+
     cloud      = new PointCloud;
     cloud->pts = deformationNodesPosition;
     kdTree     = std::make_shared<kd_tree_t>(3, *cloud, nanoflann::KDTreeSingleIndexAdaptorParams(10));
@@ -55,6 +64,42 @@ std::vector<std::shared_ptr<Node>> Warpfield::findNeighbors(int numNeighbor, std
         neighborNodes.push_back(nodes[index]);
     }
     return neighborNodes;
+}
+
+/* -------------------------------------------------------------------------- */
+/* PRIVATE METHODS */
+
+/* */
+int Warpfield::getFrameNum() { return this->frameNum++; }
+
+/* */
+void Warpfield::saveToPcl(std::vector<cv::Vec3f> vectors) {
+    /* Initiate the PCL Cloud */
+    auto cloud = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
+
+    (*cloud).width  = vectors.size();
+    (*cloud).height = 1;
+
+    (*cloud).points.resize((*cloud).width * (*cloud).height);
+
+    /* Iterate through vectors */
+    for (size_t i = 0; i < vectors.size(); i++) {
+        const cv::Vec3f& pt = vectors[i];
+
+        pcl::PointXYZ point = pcl::PointXYZ();
+        point.x             = pt[0];
+        point.y             = pt[1];
+        point.z             = pt[2];
+
+        (*cloud).points[i] = point;
+    }
+
+    /* Save to PCL */
+    std::string filenameStr = ("files/PCLFrame" + std::to_string(this->getFrameNum()) + ".pcd");
+    pcl::io::savePCDFileASCII(filenameStr, (*cloud));
+
+    /* Print out to stderr when after successful save */
+    std::cout << "Saved " << (*cloud).points.size() << " data points to " << filenameStr << std::endl;
 }
 
 // void Warpfield::addNode(Node newNode) { nodes.emplace_back(newNode); }
