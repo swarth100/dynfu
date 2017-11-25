ed2c24c18df0ac52742410beabbe36722d6588f2
Alberto Spina <as12015@ic.ac.uk>
2017-11-25 11:24:22 +0000

    cmake/CodeCoverage.cmake: Add logging of coverage data

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -156,9 +156,15 @@ function(SETUP_TARGET_FOR_COVERAGE)
 
         # Capturing lcov counters and generating report
         COMMAND ${LCOV_PATH} --directory . --capture --output-file ${Coverage_NAME}.info
-        # add baseline counters
+
+        # Add baseline counters
         COMMAND ${LCOV_PATH} -a ${Coverage_NAME}.base -a ${Coverage_NAME}.info --output-file ${Coverage_NAME}.total
         COMMAND ${LCOV_PATH} --remove ${Coverage_NAME}.total ${COVERAGE_EXCLUDES} --output-file ${PROJECT_BINARY_DIR}/${Coverage_NAME}.info.cleaned
+
+        # Log data to logfile for post-processing
+        COMMAND ${LCOV_PATH} --summary ${PROJECT_BINARY_DIR}/${Coverage_NAME}.info.cleaned > ${PROJECT_BINARY_DIR}/coverageData.txt 2>&1
+
+        # Generate HTML view
         COMMAND ${GENHTML_PATH} -o ${Coverage_NAME} ${PROJECT_BINARY_DIR}/${Coverage_NAME}.info.cleaned
         COMMAND ${CMAKE_COMMAND} -E remove ${Coverage_NAME}.base ${Coverage_NAME}.info ${Coverage_NAME}.total ${PROJECT_BINARY_DIR}/${Coverage_NAME}.info.cleaned
 
