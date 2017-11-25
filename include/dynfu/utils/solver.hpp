5996c7d4202c86d3e8bab0b9b2e285ed4687e3dc
Daniel Grzech <dg17@cdt062-lap.isd.kcl.ac.uk>
2017-11-25 14:15:33 +0000

    include/dynfu/utils/solver.hpp: Fix Optimisation of the warp field

--- include/dynfu/utils/solver.hpp
+++ include/dynfu/utils/solver.hpp
@@ -8,22 +8,22 @@
 /* ceres includes */
 #include <ceres/ceres.h>
 
+#include <iostream>
+
 #define KNN 8
 /* */
 template <class T>
 class Solver {
 private:
-    static T const* const* parameters;
+    T* parameters;
 
 public:
-    Solver();
-    ~Solver();
+    Solver()  = default;
+    ~Solver() = default;
 
-    static ceres::CostFunction* Create(std::shared_ptr<Warpfield> warpfield, std::shared_ptr<Frame> canonicalFrame,
+    static ceres::CostFunction* Create(Warpfield warpfield, std::shared_ptr<Frame> canonicalFrame,
                                        std::shared_ptr<Frame> liveFrame) {
-        auto costFunctor   = new CostFunctor<float>(warpfield, canonicalFrame, liveFrame);
-        Solver::parameters = costFunctor->getParameters();
-        auto costFunction  = new ceres::DynamicAutoDiffCostFunction<CostFunctor<float>, 3>(costFunctor);
+        auto costFunction = new ceres::DynamicAutoDiffCostFunction<CostFunctor, 3>(new CostFunctor);
 
         int numResiduals = 3;
         int noParameters = KNN * 6;
@@ -37,19 +37,32 @@ public:
         return costFunction;
     }
 
-    void calculateWarpToLive(std::shared_ptr<Warpfield> warpfield, std::shared_ptr<Frame> canonicalFrame,
+    void calculateWarpToLive(Warpfield warpfield, std::shared_ptr<Frame> canonicalFrame,
                              std::shared_ptr<Frame> liveFrame) {
-        ceres::CostFunction* costFunction = this->Create(warpfield, canonicalFrame, liveFrame);
-
         ceres::Problem problem;
-        problem.AddResidualBlock(costFunction, NULL, parameters);
 
+        double x[] = {double(parameters[0]), double(parameters[1]), double(parameters[2])};
+        problem.AddResidualBlock(Create(warpfield, canonicalFrame, liveFrame), NULL, x);
+
+        ceres::Solver::Options options;
+        options.linear_solver_type = ceres::SPARSE_NORMAL_CHOLESKY;
         ceres::Solver::Summary summary;
 
-        ceres::Solve(this->options, &problem, &summary);
+        ceres::Solve(options, &problem, &summary);
 
-        std::cout << summary.FullReport() << "\n";
+        // set the transformations to the parametres obtained from ceres
+        auto nodes = warpfield.getNodes();
+
+        int i = 0;
+        for (auto node : nodes) {
+            auto transformation = std::make_shared<DualQuaternion<float>>(0.f, 0.f, 0.f, 0.f, 0.f, 0.f);
+            node->setTransformation(transformation);
+
+            i++;
+        }
     }
+
+    // std::cout << summary.FullReport() << "\n";
 };
 
 /* DYNFU_SOLVER_HPP */
