c409d91fc40dd72249bc3b1bacc9e14a169c3dbb
Daniel Grzech <dg17@cdt062-lap.isd.kcl.ac.uk>
2017-11-25 14:29:56 +0000

    src/dynfu/warp_field.cpp: Fix Merge conflicts

--- src/dynfu/warp_field.cpp
+++ src/dynfu/warp_field.cpp
@@ -59,13 +59,8 @@ float** Warpfield::getParameters() { return parametres; }
 std::vector<std::shared_ptr<Node>> Warpfield::getNodes() {}
 
 /* Find the nodes index of k closest neighbour for the given point */
-<<<<<<< 2014235ae82d8e69e95cdbfae592fd310b1c9f3b
-std::vector<std::shared_ptr<Node>> Warpfield::findNeighbors(int numNeighbor, std::shared_ptr<Node> node) {
-    auto point = node->getPosition();
-=======
 std::vector<std::shared_ptr<Node>> Warpfield::findNeighbors(int numNeighbor, cv::Vec3f vertex) {
     // std::cout << "point" << vertex[0] << " " << vertex[1] << " " << vertex[2] << std::endl;
->>>>>>> src/dynfu/warp_field.cpp: Fix Initialisation of nearest neighbours
     /* Not used, ignores the distance to the nodes for now */
     std::vector<float> outDistSqr(numNeighbor);
     std::vector<size_t> retIndex(numNeighbor);
@@ -80,21 +75,6 @@ std::vector<std::shared_ptr<Node>> Warpfield::findNeighbors(int numNeighbor, cv:
     return neighborNodes;
 }
 
-/* Find the nodes index of k closest neighbour for the given point */
-std::vector<std::shared_ptr<Node>> Warpfield::findNeighbors(int numNeighbor, cv::Vec3f point) {
-    /* Not used, ignores the distance to the nodes for now */
-    std::vector<float> outDistSqr(numNeighbor);
-    std::vector<size_t> retIndex(numNeighbor);
-    /* Unpack the Vec3f into vector */
-    std::vector<float> query = {point[0], point[1], point[2]};
-    int n                    = kdTree->knnSearch(&query[0], numNeighbor, &retIndex[0], &outDistSqr[0]);
-    retIndex.resize(n);
-    std::vector<std::shared_ptr<Node>> neighborNodes;
-    for (auto index : retIndex) {
-        neighborNodes.push_back(nodes[index]);
-    }
-    return neighborNodes;
-}
 /* -------------------------------------------------------------------------- */
 /* PRIVATE METHODS */
 
