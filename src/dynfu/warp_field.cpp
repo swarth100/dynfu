ee29e8b77e8c1fc92bfbee9071f1862583776236
Daniel Grzech <dg17@cdt062-lap.isd.kcl.ac.uk>
2017-11-25 14:17:01 +0000

    src/dynfu/warp_field.cpp: Fix Initialisation of nearest neighbours

--- src/dynfu/warp_field.cpp
+++ src/dynfu/warp_field.cpp
@@ -20,7 +20,7 @@ Warpfield::~Warpfield() {
 }
 
 /* TODO: Add comment */
-void Warpfield::init(std::vector<std::shared_ptr<Node>>& nodes) {
+void Warpfield::init(std::vector<std::shared_ptr<Node>> nodes) {
     // initialise all deformation nodes
     this->nodes = nodes;
 
@@ -40,12 +40,18 @@ void Warpfield::init(std::vector<std::shared_ptr<Node>>& nodes) {
     kdTree->buildIndex();
 }
 
-void Warpfield::warp(std::shared_ptr<Frame> /*liveFrame*/) {
+void Warpfield::warp(std::shared_ptr<Frame> liveFrame) {
     // calculate DQB for all points
     // warps all points
-    ceres::Solver::Options options;
-    options.linear_solver_type = ceres::SPARSE_NORMAL_CHOLESKY;
+    auto points = liveFrame->getVertices();
+
+    for (auto point : points) {
+        // auto transformation = point.calculateDQB();
+        // point->setTransformation(transformation);
+    }
 }
+
+float** Warpfield::getParameters() { return parametres; }
 
 /*
  * Returns a vector of all nodes in the warp field.
@@ -53,13 +59,18 @@ void Warpfield::warp(std::shared_ptr<Frame> /*liveFrame*/) {
 std::vector<std::shared_ptr<Node>> Warpfield::getNodes() {}
 
 /* Find the nodes index of k closest neighbour for the given point */
+<<<<<<< 2014235ae82d8e69e95cdbfae592fd310b1c9f3b
 std::vector<std::shared_ptr<Node>> Warpfield::findNeighbors(int numNeighbor, std::shared_ptr<Node> node) {
     auto point = node->getPosition();
+=======
+std::vector<std::shared_ptr<Node>> Warpfield::findNeighbors(int numNeighbor, cv::Vec3f vertex) {
+    // std::cout << "point" << vertex[0] << " " << vertex[1] << " " << vertex[2] << std::endl;
+>>>>>>> src/dynfu/warp_field.cpp: Fix Initialisation of nearest neighbours
     /* Not used, ignores the distance to the nodes for now */
     std::vector<float> outDistSqr(numNeighbor);
     std::vector<size_t> retIndex(numNeighbor);
     /* Unpack the Vec3f into vector */
-    std::vector<float> query = {point[0], point[1], point[2]};
+    std::vector<float> query = {vertex[0], vertex[1], vertex[2]};
     int n                    = kdTree->knnSearch(&query[0], numNeighbor, &retIndex[0], &outDistSqr[0]);
     retIndex.resize(n);
     std::vector<std::shared_ptr<Node>> neighborNodes;
