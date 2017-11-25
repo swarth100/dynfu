a807e2578ffe6cb149bff3b3c66436135e3c6889
Daniel Grzech <dg17@cdt062-lap.isd.kcl.ac.uk>
2017-11-25 14:16:39 +0000

    src/dynfu/utils/node.cpp: Remove Calculation of nearest neighbours

--- src/dynfu/utils/node.cpp
+++ src/dynfu/utils/node.cpp
@@ -17,9 +17,7 @@ const float Node::getWeight() { return dg_w; }
 
 const std::shared_ptr<DualQuaternion<float>>& Node::getTransformation() { return dg_se3; };
 
-const std::vector<std::shared_ptr<Node>>& Node::getNearestNeighbours() { return nearestNeighbours; };
-
-void Node::setNeighbours(std::vector<std::shared_ptr<Node>> nodes) { nearestNeighbours = nodes; }
+void Node::setTransformation(std::shared_ptr<DualQuaternion<float>> transformation) { dg_se3 = transformation; }
 
 // TODO(dig15): finish once nearest neighbours have been initialised
 void Node::setWeight() {
