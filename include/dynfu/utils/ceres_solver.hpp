b304b8026db18c85f54bfaa957ecb57d7aa79ad3
Daniel Grzech <dg17@cdt062-lap.isd.kcl.ac.uk>
2017-11-28 13:39:28 +0000

    include/dynfu/utils/ceres_solver.hpp: Add Automatically differentiated dynamic cost function

--- include/dynfu/utils/ceres_solver.hpp
+++ include/dynfu/utils/ceres_solver.hpp
@@ -35,12 +35,15 @@ public:
 
         int i = 0;
         for (auto node : liveVertexNeighbours) {
-            T temp[4] = {T(transformationParameters[i][0]), T(transformationParameters[i][1]),
-                         T(transformationParameters[i][2]), T(transformationParameters[i][3])};
+            std::cout << "i: " << i << std::endl;
+            T temp[4] = {T(*transformationParameters[0]), T(*transformationParameters[1]),
+                         T(*transformationParameters[2]), T(*transformationParameters[3])};
 
             total_translation[0] += T(sourceVertex[0]) + T(temp[1] * temp[0]);
             total_translation[1] += T(sourceVertex[1]) + T(temp[2] * temp[0]);
             total_translation[2] += T(sourceVertex[2]) + T(temp[3] * temp[0]);
+
+            i++;
         }
 
         residual[0] = T(liveVertex[0]) - total_translation[0];
@@ -54,16 +57,12 @@ public:
      * factory to hide the construction of the CostFunction object from
      * the client code
      */
-    static ceres::CostFunction* Create(Warpfield warpfield, cv::Vec3f sourceVertex, cv::Vec3f liveVertex,
-                                       std::vector<double>* values, std::vector<double*>* parameter_blocks) {
-        auto cost_function =
-            new ceres::DynamicAutoDiffCostFunction<Energy, 4>(new Energy(warpfield, sourceVertex, liveVertex));
+    static ceres::CostFunction* Create(Warpfield warpfield, cv::Vec3f sourceVertex, cv::Vec3f liveVertex) {
+        auto cost_function = new ceres::DynamicAutoDiffCostFunction<Energy, 4 /* stride */>(
+            new Energy(warpfield, sourceVertex, liveVertex));
 
         for (int i = 0; i < KNN; i++) {
-            parameter_blocks->clear();
-            parameter_blocks->push_back(&((*values)[i]));
-
-            cost_function->AddParameterBlock(1);  // 3 for transl. + 1 for weights
+            cost_function->AddParameterBlock(4);
         }
 
         cost_function->SetNumResiduals(3);  // dimensionality of the residual
@@ -97,40 +96,38 @@ public:
         ceres::Problem problem;
         ceres::Solver::Summary summary;
 
-        std::vector<double> values;
+        ceres::CostFunction* cost_function;
 
-        int j                                    = 0;
-        std::vector<std::shared_ptr<Node>> nodes = warpfield.getNodes();
-
-        std::cout << ">>entering" << std::endl;
-        std::cout << "size: " << nodes.size() << std::endl;
-
-        for (auto node : nodes) {
-            j++;  // std::cout << node->getWeight() << std::endl;
-            // values.push_back(node->getWeight());
-            std::cout << j << std::endl;
-        }
-        std::cout << "<<out" << std::endl;
+        std::vector<double*> values;
 
         int i = 0;
-        for (auto vertices : liveFrame->getVertices()) {
-            std::vector<double*> parameter_blocks;
-            ceres::CostFunction* cost_function =
-                Energy::Create(warpfield, vertices, canonicalFrame->getVertices()[i], &values, &parameter_blocks);
+        for (auto vertex : liveFrame->getVertices()) {
+            auto neighbours = warpfield.findNeighbors(8, vertex);
 
-            problem.AddResidualBlock(cost_function, NULL, parameter_blocks);
+            for (auto neighbour : neighbours) {
+                printf("neigh: %p; pos: %f\n", neighbour, neighbour->getPosition()[0]);
+                printf("params: %p\n", neighbour->getParams());
+                values.emplace_back(neighbour->getParams());
+            }
+
+            cost_function = Energy::Create(warpfield, vertex, canonicalFrame->getVertices()[i]);
+            problem.AddResidualBlock(cost_function, NULL, values);
 
             i++;
         }
 
+        std::cout << "Dan's fault" << std::endl;
+
         Solve(options, &problem, &summary);
+
+        std::cout << "----- FULL REPORT -------" << std::endl;
         std::cout << summary.FullReport() << "\n";
 
-        // problem.GetParameterBlocks(&parameters);
-        //
-        // for (auto parameter : parameters) {
-        //     std::cout << *parameter << std::endl;
-        // }
+        problem.GetParameterBlocks(&parameters);
+
+        for (auto parameter : parameters) {
+            std::cout << *parameter << std::endl;
+        }
     }
 
 private:
@@ -138,4 +135,3 @@ private:
 
     std::vector<double*> parameters;
 };
-
