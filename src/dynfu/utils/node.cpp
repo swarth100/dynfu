fd068442997cb92f421cd3fa405054a777d51116
Daniel Grzech <dg17@cdt062-lap.isd.kcl.ac.uk>
2017-11-28 13:39:40 +0000

    src/dynfu/utils/node.cpp: Add Node parameters

--- src/dynfu/utils/node.cpp
+++ src/dynfu/utils/node.cpp
@@ -7,11 +7,19 @@ Node::Node(cv::Vec3f position, std::shared_ptr<DualQuaternion<float>> transforma
     this->dg_v   = position;
     this->dg_se3 = transformation;
     this->dg_w   = weight;
+
+    double temp1 = transformation->getTranslation()[0];
+    double temp2 = transformation->getTranslation()[1];
+    double temp3 = transformation->getTranslation()[2];
+
+    this->params = new double[4]{weight, temp1, temp2, temp3};
 }
 
 Node::~Node() = default;
 
 cv::Vec3f Node::getPosition() { return dg_v; }
+
+double* Node::getParams() { return params; }
 
 float Node::getWeight() { return dg_w; }
 
