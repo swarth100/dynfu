1252e0018d88481ae02f858da5e0497f397ce0cc
Daniel Grzech <dgrzech@rapidminer.com>
2017-11-29 23:59:06 +0000

    src/dynfu/utils/node.cpp: Methods relating to calculation of the transformation weight to node class

--- src/dynfu/utils/node.cpp
+++ src/dynfu/utils/node.cpp
@@ -23,6 +23,27 @@ std::shared_ptr<DualQuaternion<float>>& Node::getTransformation() { return dg_se
 
 float Node::getRadialBasisWeight() { return dg_w; }
 
+float Node::getTransformationWeight(cv::Vec3f vertexPosition) {
+    return getTransformationWeightT<float>(vertexPosition);
+}
+
+template <typename T>
+T Node::getTransformationWeightT(cv::Vec3f vertexPosition) {
+    auto position = this->getPosition();
+    auto weight   = this->getRadialBasisWeight();
+
+    cv::Vec<T, 3> distance_vec = cv::Vec<T, 3>(abs(T(position[0]) - T(vertexPosition[0])), abs(T(position[1]) - T(vertexPosition[1])),
+                                               abs(T(position[2]) - T(vertexPosition[2])));
+    T distance_norm = sqrt(abs(pow(distance_vec[0], 2.0) + pow(distance_vec[1], 2.0) + pow(distance_vec[2], 2.0)));
+
+    // if the node and the vertex are in the same place, return 1
+    if (distance_norm == T(0.0)) {
+        return T(1.0);
+    }
+
+    return exp((-1.0 * pow(distance_norm, 2)) / (2.0 * pow(weight, 2)));
+}
+
 double* Node::getParams() { return params; }
 
 void Node::setTranslation(cv::Vec3f translation) {
