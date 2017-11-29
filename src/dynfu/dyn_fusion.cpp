df7d8a901f03f5504d6a74d7bc1e7847a6f274ff
Daniel Grzech <dgrzech@rapidminer.com>
2017-11-29 23:39:18 +0000

    src/dynfu/dyn_fusion.cpp: Refactor Methods in pipeline relating to the radial basis weight of a node

--- src/dynfu/dyn_fusion.cpp
+++ src/dynfu/dyn_fusion.cpp
@@ -1,7 +1,8 @@
 #include <dynfu/dyn_fusion.hpp>
 
 DynFusion::DynFusion() = default;
-/* We initialise the dynamic fusion with the initals vertices and normals */
+
+/* initialise dynamicfusion with the initals vertices and normals */
 void DynFusion::init(kfusion::cuda::Cloud &vertices) {
     cv::Mat cloudHost = cloudToMat(vertices);
     std::vector<cv::Vec3f> canonical(cloudHost.rows * cloudHost.cols);
@@ -57,7 +58,7 @@ std::shared_ptr<DualQuaternion<float>> DynFusion::calcDQB(cv::Vec3f point) {
     /* Apply the formula to get w(x) */
     DualQuaternion<float> transformationSum(0.f, 0.f, 0.f, 0.f, 0.f, 0.f);
     for (auto node : nearestNeighbors) {
-        float nodeWeight = getWeight(node, point);
+        float nodeWeight = getTransformationWeight(node, point);
 
         DualQuaternion<float> dg_se3                  = *node->getTransformation();
         DualQuaternion<float> weighted_transformation = dg_se3 * nodeWeight;
@@ -97,6 +98,6 @@ std::vector<cv::Vec3f> DynFusion::matToVector(cv::Mat matrix) {
 }
 
 /* calculate the transformation weight using the position in the canonical frame and the radial weight of the node */
-float DynFusion::getWeight(std::shared_ptr<Node> node, cv::Vec3f point) {
-    return getWeightT<float>(node->getPosition(), node->getRadialBasisWeight(), point);
+float DynFusion::getTransformationWeight(std::shared_ptr<Node> node, cv::Vec3f point) {
+    return getTransformationWeightT<float>(node->getPosition(), node->getRadialBasisWeight(), point);
 }
