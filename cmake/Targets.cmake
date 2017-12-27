dff0f577be850e8388db1653887a735d3a88b7b0
as12015 <as12015@ic.ac.uk>
2017-12-27 15:36:17 +0000

    CMakeLists.txt: Refactor PCL inclusion to now be back on the cmake top-level
    Add PCL top-level implementation
    Add VTK wrong flag silencer
    Remove PCL sub-addition
    Remove PCL sub-addition
    Remove PCL sub-addition
    Remove PCL sub-addition

--- cmake/Targets.cmake
+++ cmake/Targets.cmake
@@ -113,3 +113,18 @@ macro(ADD_PCL)
   link_directories(${PCL_LIBRARY_DIRS})
   add_definitions(${PCL_DEFINITIONS})
 endmacro()
+
+################################################################################################
+# Remove vtk definitions
+# This is used for CUDA targets, because nvcc does not like VTK 6+ definitions
+# style.
+macro(REMOVE_VTK_DEFINITIONS)
+    get_directory_property(_dir_defs DIRECTORY ${CMAKE_SOURCE_DIR} COMPILE_DEFINITIONS)
+    set(_vtk_definitions)
+    foreach(_item ${_dir_defs})
+        if(_item MATCHES "vtk*")
+            list(APPEND _vtk_definitions -D${_item})
+        endif()
+    endforeach()
+    remove_definitions(${_vtk_definitions})
+endmacro(REMOVE_VTK_DEFINITIONS)
